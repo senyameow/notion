530d64cd13dbb89e9ad847984cd1b549b88ba894
senyameow <arseniyxd@gmail.com>
2023-10-12 21:11:58 +0700

    landing layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,12 @@
 import Heading from "./_components/Heading";
+import Heroes from "./_components/Heroes";
 
 export default function Home() {
   return (
     <div className="min-h-full flex flex-col">
       <div className="flex flex-col items-center justify-center flex-1 md:justify-start gap-y-8 text-center px-6 pb-10">
         <Heading />
+        <Heroes />
       </div>
       <div>
         footer
