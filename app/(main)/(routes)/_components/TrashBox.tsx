80116ed7234efc7664552d0412f68535265a1f47
senyameow <arseniyxd@gmail.com>
2023-10-17 01:55:40 +0700

    add search trashBox

--- app/(main)/(routes)/_components/TrashBox.tsx
+++ app/(main)/(routes)/_components/TrashBox.tsx
@@ -1,14 +1,19 @@
+'use client'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Doc as DocType } from '@/convex/_generated/dataModel'
-import React from 'react'
+import React, { useState } from 'react'
 import Doc from './Doc'
 import TrashItem from './TrashItem'
+import { Search } from 'lucide-react'
+import { Input } from '@/components/ui/input'
 
 interface TrashBoxProps {
     docs: DocType<'documents'>[] | undefined
 }
 
 const TrashBox = ({ docs }: TrashBoxProps) => {
+
+    const [search, setSearch] = useState('')
 
     if (docs === undefined) {
         return (
@@ -19,13 +24,20 @@ const TrashBox = ({ docs }: TrashBoxProps) => {
     }
 
     return (
-        <ScrollArea className='w-full h-[230px]'>
-            <div className='flex flex-col w-full'>
-                {docs.map(doc => (
-                    <TrashItem doc={doc} />
-                ))}
+        <div className='flex flex-col w-full h-full'>
+            <div className='flex items-center gap-1 p-2 relative border py-0 rounded-lg mb-2 w-full'>
+                <Search className='w-4 h-4' />
+                <Input value={search} placeholder='filter by title...' className='border-0 focus-visible:ring-0 focus-visible:ring-offset-0 ring-0 ring-offset-0' onChange={e => setSearch(e.target.value)} />
             </div>
-        </ScrollArea>
+
+            {docs.length > 0 ? < ScrollArea className='w-full h-[230px]'>
+                <div className='flex flex-col w-full'>
+                    {docs.map(doc => (
+                        <TrashItem doc={doc} />
+                    ))}
+                </div>
+            </ScrollArea> : <p className='text-neutral-500 text-sm text-center hidden last:block font-semibold py-4'>No documents found..</p>}
+        </div >
     )
 }
 
