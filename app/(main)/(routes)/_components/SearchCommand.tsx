79cf766f9bd1b3c20721cd6f1e50c17e9e5164e9
senyameow <arseniyxd@gmail.com>
2023-10-17 23:47:35 +0700

    add search-command component

---
+++ app/(main)/(routes)/_components/SearchCommand.tsx
@@ -0,0 +1,38 @@
+'use client'
+import React from 'react'
+
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+    CommandShortcut,
+} from "@/components/ui/command"
+
+const SearchCommand = () => {
+
+    return (
+        <Command>
+            <CommandInput placeholder="Type a command or search..." />
+            <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                <CommandGroup heading="Suggestions">
+                    <CommandItem>Calendar</CommandItem>
+                    <CommandItem>Search Emoji</CommandItem>
+                    <CommandItem>Calculator</CommandItem>
+                </CommandGroup>
+                <CommandSeparator />
+                <CommandGroup heading="Settings">
+                    <CommandItem>Profile</CommandItem>
+                    <CommandItem>Billing</CommandItem>
+                    <CommandItem>Settings</CommandItem>
+                </CommandGroup>
+            </CommandList>
+        </Command>
+    )
+}
+
+export default SearchCommand
