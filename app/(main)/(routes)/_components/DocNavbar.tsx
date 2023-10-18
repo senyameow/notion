9de7876b3c06a930b93482f130f34a4589bb46dc
senyameow <arseniyxd@gmail.com>
2023-10-18 22:28:27 +0700

    add dropdown DocNavbar

--- app/(main)/(routes)/_components/DocNavbar.tsx
+++ app/(main)/(routes)/_components/DocNavbar.tsx
@@ -2,12 +2,26 @@
 import { Skeleton } from '@/components/ui/skeleton'
 import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
-import { useQuery } from 'convex/react'
-import { Menu } from 'lucide-react'
+import { useMutation, useQuery } from 'convex/react'
+import { ArchiveRestore, Info, Loader2, Menu, MessageCircle, MoreHorizontal, Trash } from 'lucide-react'
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
 import Title from './Title'
+import { Button } from '@/components/ui/button'
 
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useUser } from '@clerk/clerk-react'
+import { toast } from 'sonner'
 interface DocNavbarProps {
     isCollapsed: boolean;
     onResetWidth: () => void;
@@ -16,6 +30,8 @@ interface DocNavbarProps {
 const DocNavbar = ({ isCollapsed, onResetWidth }: DocNavbarProps) => {
 
     const params = useParams()
+
+    const [isDeleting, setIsDeleting] = useState(false)
 
     const doc = useQuery(api.documents.getNote, { id: params.docId as Id<"documents"> })
 
@@ -29,6 +45,33 @@ const DocNavbar = ({ isCollapsed, onResetWidth }: DocNavbarProps) => {
 
     if (doc === null) return null
 
+    const archieve = useMutation(api.documents.archiveDoc)
+    const onArchieve = async (e: Event) => {
+        try {
+            e.stopPropagation()
+            e.preventDefault()
+            setIsDeleting(true)
+            await archieve({ docId: doc._id })
+            toast.success(`you've deleted ${doc.title}`)
+        } catch (error) {
+            toast.error('something went wrong')
+        } finally {
+            setIsDeleting(false)
+        }
+    }
+
+    const restore = useMutation(api.documents.restore)
+
+    const onRestore = () => {
+        const promise = restore({ id: doc._id })
+        toast.promise(promise, {
+            loading: 'Restoring note..',
+            success: 'Note restored',
+            error: 'Something went wrong'
+        })
+
+    }
+
     return (
         <div className='p-3 py-5 pr-5 w-full bg-background dark:bg-dark'>
             {isCollapsed ? (
@@ -38,8 +81,38 @@ const DocNavbar = ({ isCollapsed, onResetWidth }: DocNavbarProps) => {
                     <div>
                         <Title initialDoc={doc} />
                     </div>
-                    <div>
-                        publish
+                    <div className='flex items-center gap-2'>
+                        <Button className='' variant={'ghost'}>Publish</Button>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                <MoreHorizontal role='button' className='w-8 h-8 p-2 hover:bg-neutral-100 dark:hover:bg-neutral-700/80 rounded-lg' />
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="start" alignOffset={30} forceMount className="w-48">
+                                <DropdownMenuLabel>{String(doc.title)}</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuGroup className="flex items-center p-1 flex-col">
+                                    <DropdownMenuItem className="cursor-pointer hover:opacity-90 w-full">
+                                        <Info className='w-4 h-4 mr-2' />
+                                        Info
+                                    </DropdownMenuItem>
+                                    {doc.isAcrchieved ? (
+                                        <DropdownMenuItem className="cursor-pointer hover:opacity-90 w-full" onSelect={onRestore}>
+                                            <ArchiveRestore className='w-4 h-4 mr-2' />
+                                            Restore
+                                        </DropdownMenuItem>
+                                    ) : <DropdownMenuItem disabled={isDeleting} onSelect={onArchieve} className="cursor-pointer hover:opacity-90 w-full">
+                                        {isDeleting ? <Loader2 className='w-4 h-4 mr-2 animate-spin' /> : (
+                                            <Trash className='w-4 h-4 mr-2' />
+                                        )}
+                                        Delete
+                                    </DropdownMenuItem>}
+                                    <DropdownMenuItem className="cursor-pointer hover:opacity-90 w-full">
+                                        <MessageCircle className='w-4 h-4 mr-2' />
+                                        Comments
+                                    </DropdownMenuItem>
+                                </DropdownMenuGroup>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                     </div>
                 </div>
             )}
