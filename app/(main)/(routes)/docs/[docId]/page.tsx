6c6925c398562cf37716eed558bfa98b9b12c0b2
senyameow <arseniyxd@gmail.com>
2023-10-18 22:32:51 +0700

    fix params_type page

--- app/(main)/(routes)/docs/[docId]/page.tsx
+++ app/(main)/(routes)/docs/[docId]/page.tsx
@@ -5,13 +5,13 @@ import { Id } from '@/convex/_generated/dataModel'
 import { useQuery } from 'convex/react'
 import React from 'react'
 
-const DocPage = ({ params }: { params: { docId: string } }) => {
+const DocPage = ({ params }: { params: { docId: Id<'documents'> } }) => {
 
-    const doc = useQuery(api.documents.getNote, { id: params.docId as Id<'documents'> })
+    const doc = useQuery(api.documents.getNote, { id: params.docId })
 
     return (
         <div className='pt-20'>
-            {doc?.isAcrchieved && <Banner text='this note has been archived' docId={params.docId as Id<'documents'>} />}
+            {doc?.isAcrchieved && <Banner text='this note has been archived' docId={params.docId} />}
             {params.docId}
         </div>
     )
