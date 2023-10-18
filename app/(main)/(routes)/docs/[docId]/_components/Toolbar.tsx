e1d24a5f662f0c3e5bff6b1631a74a9b5c96b408
senyameow <arseniyxd@gmail.com>
2023-10-19 00:04:47 +0700

    change picker

--- app/(main)/(routes)/docs/[docId]/_components/Toolbar.tsx
+++ app/(main)/(routes)/docs/[docId]/_components/Toolbar.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 import { api } from '@/convex/_generated/api';
 import { Doc } from '@/convex/_generated/dataModel'
 import { useMutation } from 'convex/react';
-import { Loader2, Smile } from 'lucide-react'
+import { Image, Loader2, Smile, X } from 'lucide-react'
 import React from 'react'
 
 interface ToolbarProps {
@@ -34,18 +34,41 @@ const Toolbar = ({
             icon: emoji
         })
     }
+    const onRemoveIcon = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.stopPropagation()
+        update({
+            id: initialDoc._id,
+            icon: ''
+        })
+    }
 
     return (
         <div className='flex flex-col gap-3 items-start group'>
-            {!!initialDoc && !preview && <div className='flex flex-row items-center gap-1 group/icons pt-6'>
-                <EmojiPicker onChange={onChangeIcon}>
-                    <Button>
-                        <Smile className='w-3 h-3 mr-2' />
+            {!preview && <div className='flex flex-row items-center gap-2 group/icons pt-6 opacity-0 group-hover:opacity-100 transition'>
+                {!initialDoc.icon && <EmojiPicker onChange={onChangeIcon}>
+                    {<Button variant={'ghost'} className='border-[0.7px] border-neutral-100'>
+                        <Smile className='w-4 h-4 mr-2' />
                         Add Icon
+                    </Button>}
+                </EmojiPicker>}
+                {!initialDoc.cover_image && (
+                    <Button className='border-[0.7px] border-neutral-100' variant={'ghost'}>
+                        <Image className='w-4 h-4 mr-2' />
+                        Add cover
                     </Button>
-                </EmojiPicker>
-                <div>add image</div>
+                )}
             </div>}
+            {!!initialDoc.icon && !preview && (
+                <EmojiPicker onChange={onChangeIcon}>
+                    <div className='text-6xl py-6 relative'>
+                        {initialDoc.icon}
+                        <Button onClick={onRemoveIcon} variant={'destructive'} className='rounded-full h-fit p-1 hover:opacity-90 absolute top-5 right-3'>
+                            <X className='w-4 h-4' />
+                        </Button>
+                    </div>
+                </EmojiPicker>
+            )}
+
             <div>
                 {initialDoc.title}
             </div>
