8e7ee367b6e80e859e4cc759df9f11951bc43770
senyameow <arseniyxd@gmail.com>
2023-11-01 21:26:05 +0700

    add skeleton

--- app/(main)/(routes)/_components/Comment.tsx
+++ app/(main)/(routes)/_components/Comment.tsx
@@ -17,6 +17,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 interface CommentProps {
     comment: Doc<'comments'>
@@ -28,11 +29,16 @@ const Comment = ({ comment }: CommentProps) => {
 
     return (
         <>
-            {commentCreater === undefined ? <Skeleton className='w-[300px] h-[150px] bg-gray-500' /> : <Card className="w-[300px] min-h-[150px]">
+            {commentCreater === undefined ? <Comment.Skeleton /> : <Card className="w-[300px] min-h-[150px]">
                 <CardHeader>
-                    <CardTitle>Create project</CardTitle>
+                    <CardTitle>
+                        <div className='w-full flex items-center gap-3'>
+                            <Image src={commentCreater.image_url} alt='avatar' className='rounded-full' width={30} height={30} />
+                            <span className='text-lg font-semibold'>{commentCreater.name}</span>
+                        </div >
+                    </CardTitle >
                     <CardDescription>Deploy your new project in one-click.</CardDescription>
-                </CardHeader>
+                </CardHeader >
                 <CardContent>
                     <form>
                         <div className="grid w-full items-center gap-4">
@@ -47,8 +53,23 @@ const Comment = ({ comment }: CommentProps) => {
                     <Button variant="outline">Cancel</Button>
                     <Button>Close</Button>
                 </CardFooter>
-            </Card>}
+            </Card >}
         </>
+    )
+}
+
+Comment.Skeleton = function ItemSkeleton() {
+    return (
+        <div className='flex gap-2 items-center'>
+            <Skeleton className='h-[30px] w-[30px] rounded-full bg-gray-400 opacity-80' />
+            <div className='flex flex-col gap-2 items-start'>
+                <Skeleton className='h-2 w-[200px] rounded-md  bg-gray-400 opacity-80' />
+                <Skeleton className='h-2 w-[140px] rounded-md  bg-gray-400 opacity-80' />
+                <Skeleton className='h-2 w-[170px] rounded-md  bg-gray-400 opacity-80' />
+                <Skeleton className='h-2 w-[165px] rounded-md  bg-gray-400 opacity-80' />
+                <Skeleton className='h-2 w-[180px] rounded-md  bg-gray-400 opacity-80' />
+            </div>
+        </div>
     )
 }
 
--- app/(main)/(routes)/_components/CommentSheet.tsx
+++ app/(main)/(routes)/_components/CommentSheet.tsx
@@ -17,11 +17,12 @@ import {
 import { api } from "@/convex/_generated/api"
 import { Doc } from "@/convex/_generated/dataModel"
 import { useQuery } from "convex/react"
-import { MessageCircle } from "lucide-react"
+import { MessageCircle, MessagesSquare } from "lucide-react"
 import UserCard from "./UserCard"
 import { format } from "date-fns"
 import Comment from "./Comment"
 import { Skeleton } from "@/components/ui/skeleton"
+import { cn } from "@/lib/utils"
 
 interface CommentSheetProps {
     doc: Doc<'documents'>
@@ -35,24 +36,35 @@ const CommentSheet = ({ doc }: CommentSheetProps) => {
 
     return (
         <Sheet>
-            <SheetTrigger className="flex items-center w-full px-2 py-1 hover:opacity-90 transition-colors focus:bg-accent hover:bg-accent focus:text-accent-foreground hover:text-accent-foreground rounded-md">
+            <SheetTrigger className="flex items-center w-full h-full px-2 py-1 hover:opacity-90 transition-colors focus:bg-accent hover:bg-accent focus:text-accent-foreground hover:text-accent-foreground rounded-md">
                 <MessageCircle className='w-4 h-4 mr-2' />
                 Comments
             </SheetTrigger>
-            <SheetContent className="z-[99999]">
+            <SheetContent className="z-[99999] w-full h-full flex-1">
                 <SheetHeader>
                     <SheetTitle>{doc.title}</SheetTitle>
                     <SheetDescription>
                         You can change behavior in settings
                     </SheetDescription>
                 </SheetHeader>
-                <div className="flex items-start gap-5 flex-col py-8 h-full">
+                <div className="flex items-start gap-5 flex-col py-8 h-full w-full">
                     <Label>Comments: </Label>
-                    {comments === undefined ? (
-                        <div className="w-full h-full flex-col flex gap-4">
-                            <Skeleton className="w-[300px] h-[100px]" />
+                    {comments?.length === 0 && (
+                        <div className="w-full h-full gap-2 flex flex-col items-center justify-center text-sm text-center">
+                            <MessagesSquare className="w-12 h-12" />
+                            <span className="text-gray-400">No open comments yet</span>
+                            <span className="max-w-[200px]">Open comments on this page will appear here</span>
                         </div>
-                    ) : <ScrollArea className="h-full w-full">
+                    )}
+                    {comments === undefined ? (
+                        <div className="w-full h-full flex-col flex gap-5">
+                            <Comment.Skeleton />
+                            <Comment.Skeleton />
+                            <Comment.Skeleton />
+                            <Comment.Skeleton />
+                            <Comment.Skeleton />
+                        </div>
+                    ) : <ScrollArea className={cn(`h-full w-full`, comments.length === 0 && 'hidden')}>
                         {comments?.map(comment => (
                             <Comment key={comment._id} comment={comment} />
                         ))}
