0af89bc673a7bacc61db0743bf48af7a7aa259d6
senyameow <arseniyxd@gmail.com>
2023-11-20 23:48:03 +0700

    redirect

--- app/(main)/(routes)/docs/page.tsx
+++ app/(main)/(routes)/docs/page.tsx
@@ -1,12 +1,25 @@
+'use client'
 import React from 'react'
 import Empty from './_components/Empty'
+import { useQuery } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { redirect } from 'next/navigation'
 
 const EmptyPage = () => {
-    return (
-        <div className='h-full w-full flex items-center justify-center overflow-x-hidden'>
-            <Empty />
-        </div>
-    )
+
+    const docs = useQuery(api.documents.getAllDocs)
+
+
+    if (docs === undefined || docs.length === 0) {
+        return (
+            <div className='h-full w-full flex items-center justify-center overflow-x-hidden'>
+                <Empty />
+            </div>
+        )
+    }
+
+    else return redirect(`docs/${docs.filter(doc => !doc.isAcrchieved)[docs.filter(doc => !doc.isAcrchieved).length - 1]._id}`)
+
 }
 
 export default EmptyPage
